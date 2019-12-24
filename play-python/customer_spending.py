4fa5994effe211a9e3d31b02c55b71226dc536a1
allen <theallenma@gmail.com>
2019-12-24 01:44:07 -0800

    Sort the result

--- play-python/customer_spending.py
+++ play-python/customer_spending.py
@@ -14,7 +14,10 @@ if __name__ == "__main__":
     # (customerId, spending)
     lines = lines.map(lambda line: line.split(",")).map(lambda line: (line[0], line[2]))
     
-    lines = lines.reduceByKey(lambda x, y: float(x)+ float(y)).mapValues(lambda x: round(x, 2)).collect()
+    lines = lines.reduceByKey(lambda x, y: float(x)+ float(y)).mapValues(lambda x: round(x, 2))
+
+    # sort it
+    lines = lines.map(lambda x: (x[1], x[0])).sortByKey().map(lambda x: (x[1], x[0])).collect()
 
     print(lines)
     
